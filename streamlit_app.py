a4b68498f3abd84c51df2b5a7bb538ec76200d5f
MrMabey <152029849+MrMabey@users.noreply.github.com>
2024-11-20 14:59:29 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,29 @@ import json
 #Added for google sheets
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
+import streamlit_authenticator as stauth
 
+# --- USER AUTHENTICATION ---
+names = ["Peter Parker", "Rebecca Miller"]
+usernames = ["pparker", "rmiller"]
+
+# load hashed passwords
+file_path = Path(__file__).parent / "hashed_pw.pkl"
+with file_path.open("rb") as file:
+    hashed_passwords = pickle.load(file)
+
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+    "sales_dashboard", "abcdef", cookie_expiry_days=30)
+
+name, authentication_status, username = authenticator.login("Login", "main")
+
+if authentication_status == False:
+    st.error("Username/password is incorrect")
+
+if authentication_status == None:
+    st.warning("Please enter your username and password")
+
+if authentication_status:
 
 # Page configuration
 st.set_page_config(
