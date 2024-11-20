b2be0e9f235bf11031f15599dec530d792df0211
MrMabey <152029849+MrMabey@users.noreply.github.com>
2024-11-20 15:30:42 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,17 @@ try:
     authenticator.login()
 except Exception as e:
     st.error(e)
+
+
+if st.session_state['authentication_status']:
+    authenticator.logout()
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.title('Some content')
+elif st.session_state['authentication_status'] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state['authentication_status'] is None:
+    st.warning('Please enter your username and password')
+
 # Establishing a Google Sheets connection
 conn = st.connection("gsheets", type=GSheetsConnection)
 
